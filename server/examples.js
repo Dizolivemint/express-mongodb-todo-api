be1addd07d7d906b5d26841086f464e0b4f05b76
Miles Exner <Dizolivemint@users.noreply.github.com>
2018-02-02 14:06:00 -0800

    Add POST /todos route and refactor mongoose

---
+++ server/examples.js
@@ -0,0 +1,33 @@
+// var newTodo = new Todo({
+//   text: 'Cook dinner'
+// });
+//
+// newTodo.save().then((doc) => {
+//   console.log('Saved todo', doc);
+// }, (e) => {
+//   console.log('Unable to save todo');
+// });
+
+// var secondTodo = new Todo({
+//   text: 'Bake acorn squash',
+//   completed: true,
+//   completedAt: 1200
+// });
+//
+// secondTodo.save().then((doc) => {
+//   console.log('Saved todo', doc);
+// }, (e) => {
+//   console.log('Unable to save todo')
+// });
+
+
+
+// var newUser = new User({
+//   email: ''
+// });
+//
+// newUser.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Failed to save: ', e);
+// });
