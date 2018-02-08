a6d5bcf15da70393da5126d301d1e81606ab16f6
Miles Exner <Dizolivemint@users.noreply.github.com>
2018-02-07 21:59:50 -0800

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -32,6 +32,7 @@ var UserSchema = new mongoose.Schema({
   }]
 });
 
+// Instance method with document variable
 UserSchema.methods.toJSON = function () {
   var user = this;
   var userObject = user.toObject();
@@ -48,6 +49,28 @@ UserSchema.methods.generateAuthToken = function () {
 
   return user.save().then(() => {
     return token;
+  });
+};
+
+// Model method with model
+UserSchema.statics.findByToken = function(token) {
+  var User = this;
+
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    // return new Promise((resolve, reject) => {
+    //   reject();
+    // });
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
   });
 };
 
